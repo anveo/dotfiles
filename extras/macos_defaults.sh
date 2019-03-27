40b30eab2e873a4d8e1dc8e8d9348e89a2dddeed
Brian Racer <bracer@gmail.com>
2019-03-27 07:43:39 -0600

    Disable dark mode in chrome

--- extras/macos_defaults.sh
+++ extras/macos_defaults.sh
@@ -3,4 +3,6 @@
 defaults write com.apple.dock autohide-time-modifier -int 0
 defaults write com.apple.dock autohide-delay -float 0
 
+defaults write com.google.Chrome NSRequiresAquaSystemAppearance -bool Yes
+
 killall Dock
