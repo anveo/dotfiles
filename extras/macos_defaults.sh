35b6a8fefc0707201215e391696fc7a25623b870
Brian Racer <bracer@gmail.com>
2019-12-01 16:05:21 -0700

    don't show recent apps in taskbar

--- extras/macos_defaults.sh
+++ extras/macos_defaults.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env sh
 
+# Remove the animation when hiding/showing the Dock
 defaults write com.apple.dock autohide-time-modifier -int 0
+# Remove the auto-hiding Dock delay
 defaults write com.apple.dock autohide-delay -float 0
 
 defaults write com.google.Chrome NSRequiresAquaSystemAppearance -bool Yes
+
+# Don't show recent applications in Dock
+defaults write com.apple.dock show-recents -bool false
 
 killall Dock
