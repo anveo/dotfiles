667356a13646a4ad08f4a009af428888a73c51b7
Brian Racer <bracer@gmail.com>
2020-11-17 08:16:00 -0700

    1pass expiration signal

--- zsh/spaceship-prompt/sections/op.zsh
+++ zsh/spaceship-prompt/sections/op.zsh
@@ -18,16 +18,24 @@ SPACESHIP_OP_COLOR="${SPACESHIP_OP_COLOR="blue"}"
 spaceship_op() {
   [[ $SPACESHIP_OP_SHOW == false ]] && return
 
-  op_session=`env | grep '^OP_SESSION_' | cut -d'=' -f1 | sed -e's/OP_SESSION_//g' | tr -d '\n'`
+  op_session=`env | grep -v '^OP_SESSION_EXPIRATION' | grep '^OP_SESSION_' | cut -d'=' -f1 | sed -e's/OP_SESSION_//g' | tr -d '\n'`
 
   # Check to see if a 1password session is active
   [[ -z $op_session ]] && return
+
+  # Check to see if the credentials are expired
+  OP_EXPIRED=""
+  CURRENT_TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%SZ") # `date --iso-8601=ns` does not work by default on MacOS
+
+  if [[ $CURRENT_TIMESTAMP > $OP_SESSION_EXPIRATION ]]; then
+    OP_EXPIRED=" (expired)"
+  fi
 
   # Show prompt section
   spaceship::section \
     "$SPACESHIP_OP_COLOR" \
     "$SPACESHIP_OP_PREFIX" \
-    "${SPACESHIP_OP_SYMBOL}$op_session" \
+    "${SPACESHIP_OP_SYMBOL}$op_session$OP_EXPIRED" \
     "$SPACESHIP_OP_SUFFIX"
 }
 
