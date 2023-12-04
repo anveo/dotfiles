aad15dd15a0e18b6915413ebc213a13d0da8e52c
Brian Racer <bracer@gmail.com>
2023-12-04 10:50:19 -0700

    macos sonoma fix

--- shell/pyenv.sh
+++ shell/pyenv.sh
@@ -1,7 +1,9 @@
 if [ -f $HOME/.pyenv/bin/pyenv ]; then
   export PYENV_ROOT="$HOME/.pyenv"
   export PATH="$PYENV_ROOT/bin:$PATH"
+
   eval "$(pyenv init --path)"
+  # Sonoma fix
+  eval "$(pyenv init -)"
   eval "$(pyenv virtualenv-init -)"
 fi
-
