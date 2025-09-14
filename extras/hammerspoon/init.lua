769db3030aba15abb45e1c0f4ddab54e893ac0cc
Brian Racer <bracer@gmail.com>
2025-09-14 12:30:24 -0600

    hammerspoon bindings

--- extras/hammerspoon/init.lua
+++ extras/hammerspoon/init.lua
@@ -27,20 +27,23 @@ hs.hotkey.bind(hyper, "h", hs.grid.toggleShow)
 
 -- Use Karabiner-EventViewer->Frontmost application to find bundle identifiers
 hs.hotkey.bind(hyper, "1", focusApp("com.google.Chrome"))
-hs.hotkey.bind(hyper, "2", focusApp("com.googlecode.iterm2"))
-hs.hotkey.bind(hyper, "t", focusApp("com.googlecode.iterm2"))
+hs.hotkey.bind(hyper, "2", focusApp("Todoist"))
 hs.hotkey.bind(hyper, "3", focusApp("Code")) -- com.microsoft.VSCode
-hs.hotkey.bind(hyper, "4", focusApp("notion.id"))
+
+hs.hotkey.bind(hyper, "t", focusApp("com.googlecode.iterm2"))
+
+hs.hotkey.bind(hyper, "o", focusApp("Todoist"))
+
 hs.hotkey.bind(hyper, "n", focusApp("notion.id"))
 hs.hotkey.bind(hyper, "x", focusApp("Notion Calendar")) -- com.cron.electron
-hs.hotkey.bind(hyper, "5", focusApp("Slack"))
+
 hs.hotkey.bind(hyper, "s", focusApp("Slack"))
-hs.hotkey.bind(hyper, "l", focusApp("Linear"))
-hs.hotkey.bind(hyper, "6", focusApp("Discord"))
 hs.hotkey.bind(hyper, "d", focusApp("Discord"))
-hs.hotkey.bind(hyper, "7", focusApp("Figma"))
+
+hs.hotkey.bind(hyper, "l", focusApp("Linear"))
 hs.hotkey.bind(hyper, "f", focusApp("Figma"))
-hs.hotkey.bind(hyper, "m", focusApp("Miro"))
+hs.hotkey.bind(hyper, "m", focusApp("com.electron.realtimeboard")) -- Miro
+hs.hotkey.bind(hyper, "p", focusApp("1Password"))
 hs.hotkey.bind(hyper, "g", focusApp("DataGrip"))
 hs.hotkey.bind(hyper, "z", focusApp("Zoom"))
 hs.hotkey.bind(hyper, "c", focusApp("Claude"))
