a8485c5eeea8d48e3bf63a0dc1e55ece3e29db8c
Brian Racer <bracer@gmail.com>
2025-12-09 10:26:10 -0700

    update k8s spaceship

--- zsh/spaceship-prompt/sections/k8s.zsh
+++ zsh/spaceship-prompt/sections/k8s.zsh
@@ -9,21 +9,20 @@
 # ------------------------------------------------------------------------------
 
 SPACESHIP_KUBECTL_SHOW="${SPACESHIP_KUBECTL_SHOW=false}"
+SPACESHIP_KUBECTL_ASYNC="${SPACESHIP_KUBECTL_ASYNC=true}"
 SPACESHIP_KUBECTL_PREFIX="${SPACESHIP_KUBECTL_PREFIX="at "}"
 SPACESHIP_KUBECTL_SUFFIX="${SPACESHIP_KUBECTL_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"}"
 SPACESHIP_KUBECTL_COLOR="${SPACESHIP_KUBECTL_COLOR="white"}"
-# Additional space is added because ☸️ is much bigger than the other symbols
-# See: https://github.com/denysdovhan/spaceship-prompt/pull/432
-SPACESHIP_KUBECTL_SYMBOL="${SPACESHIP_KUBECTL_SYMBOL="☸️  "}"
-
+SPACESHIP_KUBECTL_SYMBOL="${SPACESHIP_KUBECTL_SYMBOL="☸️ "}"
 # ------------------------------------------------------------------------------
 # Dependencies
 # ------------------------------------------------------------------------------
 
-# source "$SPACESHIP_ROOT/sections/kubectl_version.zsh"
-# source "$SPACESHIP_ROOT/sections/kubectl_context.zsh"
-# HACK SPACESHIP_ROOT isn't available yet
-source "$HOME/.zplug/repos/denysdovhan/spaceship-prompt/sections/kubectl_context.zsh"
+source "$SPACESHIP_ROOT/sections/kubectl_version.zsh"
+source "$SPACESHIP_ROOT/sections/kubectl_context.zsh"
+
+spaceship::precompile "$SPACESHIP_ROOT/sections/kubectl_version.zsh"
+spaceship::precompile "$SPACESHIP_ROOT/sections/kubectl_context.zsh"
 
 # ------------------------------------------------------------------------------
 # Section
@@ -35,13 +34,18 @@ source "$HOME/.zplug/repos/denysdovhan/spaceship-prompt/sections/kubectl_context
 spaceship_k8s() {
   [[ $SPACESHIP_KUBECTL_SHOW == false ]] && return
 
+  local kubectl_version="$(spaceship_kubectl_version)"
   local kubectl_context="$(spaceship_kubectl_context)"
 
-  [[ -z $kubectl_context ]] && return
+  [[ -z $kubectl_version && -z $kubectl_context ]] && return
+
+  local kubectl_version_section="$(spaceship::section::render $kubectl_version)"
+  local kubectl_context_section="$(spaceship::section::render $kubectl_context)"
 
   spaceship::section \
-    "$SPACESHIP_KUBECTL_COLOR" \
-    "$SPACESHIP_KUBECTL_PREFIX" \
-    "${SPACESHIP_KUBECTL_SYMBOL}${kubectl_context}" \
-    "$SPACESHIP_KUBECTL_SUFFIX"
+    --color "$SPACESHIP_KUBECTL_COLOR" \
+    --prefix "$SPACESHIP_KUBECTL_PREFIX" \
+    --suffix "$SPACESHIP_KUBECTL_SUFFIX" \
+    --symbol "$SPACESHIP_KUBECTL_SYMBOL" \
+    "${kubectl_version_section}${kubectl_context_section}"
 }
