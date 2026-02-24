422f8a3949a4eef28624b073f408d3acc5e49e1d
Brian Racer <bracer@gmail.com>
2026-02-24 13:57:56 -0700

    pnpm path

---
+++ shell/pnpm.sh
@@ -0,0 +1,8 @@
+# pnpm setup
+if [ -d "$HOME/Library/pnpm" ]; then
+  export PNPM_HOME="$HOME/Library/pnpm"
+  case ":$PATH:" in
+    *":$PNPM_HOME:"*) ;;
+    *) export PATH="$PNPM_HOME:$PATH" ;;
+  esac
+fi
