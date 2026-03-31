7419785d685214e33282933f16ee5a64fcff066b
Brian Racer <bracer@gmail.com>
2026-03-30 20:33:31 -0600

    extra macos defaults

--- extras/macos_defaults.sh
+++ extras/macos_defaults.sh
@@ -10,4 +10,13 @@ defaults write com.google.Chrome NSRequiresAquaSystemAppearance -bool Yes
 # Don't show recent applications in Dock
 defaults write com.apple.dock show-recents -bool false
 
+defaults write com.apple.Dock appswitcher-all-displays -bool true
+
+# Spacers
+# defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="small-spacer-tile";}'; killall Dock
+# defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="spacer-tile";}'; killall Dock
+
+mkdir  ~/Documents/Screenshots
+defaults write com.apple.screencapture location ~/Documents/Screenshots
+
 killall Dock
